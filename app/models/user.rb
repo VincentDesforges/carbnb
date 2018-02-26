32c103bcff85a46f3b8cbb5ff97c3e2445f0f0e8
Adam Smith <adamsmith098@gmail.com>
2018-02-26 17:14:45 +0000

    drafted navbar and footer components, min height container added to application layout to position footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :cars
   has_many :bookings
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :address, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :username, presence: true
+  # validates :username, uniqueness: true
+  # validates :address, presence: true
 
 end
