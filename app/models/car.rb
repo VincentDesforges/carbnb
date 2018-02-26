8d9075eb6ead6d1c67dc5616ccc074687b9e4554
Johann Goedhart <johann@goedhart.biz>
2018-02-26 15:06:05 +0000

    added validation in modles

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,13 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :mileage, presence: true
+  validates :year, presence: true
+  validates :color, presence: true
+  validates :price_per_day, presence: true
+  validates :location, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :cars
+  has_many :bookings
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :address, presence: true
+
 end
