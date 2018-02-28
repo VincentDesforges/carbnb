9d1d1486df6662a064b7d9c86c4dae2b46c9d6d8
Vincent Desforges <desforges.vincent@gmail.com>
2018-02-28 11:07:39 +0000

    Added the longitude and latitude variables to the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -11,4 +11,7 @@ class Car < ApplicationRecord
   validates :location, presence: true
 
   mount_uploader :photo, PhotoUploader
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
