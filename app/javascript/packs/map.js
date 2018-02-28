7f077601619b85fb58982a2938d96444a1330977
Vincent Desforges <desforges.vincent@gmail.com>
2018-02-28 14:20:11 +0000

    map with optional marker change

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -5,7 +5,21 @@ const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
+
+  // // Add marker options
+  // const image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+  // markers.forEach(function(marker) {
+  //   map.addMarker({
+  //     icon: image,
+  //     position: marker
+  //   }
+  //   );
+  // });
+  // // add marker option end
+
+  map.addMarkers(markers); // default marker
+
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
@@ -14,6 +28,45 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   } else {
     map.fitLatLngBounds(markers);
   }
+
+  const styles = [
+    {
+      "stylers": [
+        {
+          "hue": "#dd0d0d"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "lightness": 100
+          },
+          {
+            "visibility": "simplified"
+          }
+        ]
+    }
+  ];
+
+  map.addStyle({
+    styles: styles,
+    mapTypeId: 'map_style'
+  });
+  map.setStyle('map_style');
 }
+
+
 
 autocomplete();
